04baa0628786b84d7c37cfb7732c1c753070824f
Nikola <barbarinikola3@gmail.com>
2022-03-10 22:25:48 +0100

    initial commit

---
+++ iOS app/SofaAcademyMarch/ProfileVC.swift
@@ -0,0 +1,18 @@
+//
+//  profileVC.swift
+//  SofaAcademyMarch
+//
+//  Created by Nikola Barbarić on 10.03.2022..
+//
+
+import Foundation
+import UIKit
+
+class ProfileVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+        
+    }
+}
---
+++ iOS app/SofaAcademyMarch/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  searchVC.swift
+//  SofaAcademyMarch
+//
+//  Created by Nikola Barbarić on 10.03.2022..
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+}
