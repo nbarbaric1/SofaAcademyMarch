beff51ad4c7b5f860fd6c9f89dfed1815ee3285f
Nikola <barbarinikola3@gmail.com>
2022-03-30 00:07:59 +0200

    BaseViewController added

---
+++ iOS app/SofaAcademyMarch/Common/Base/BaseViewController.swift
@@ -0,0 +1,60 @@
+//
+//  BaseViewController.swift
+//  SofaAcademyMarch
+//
+//  Created by Nikola Barbarić on 29.03.2022..
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        title = " "
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        print("baseVC deinit")
+    }
+}
+
+// MARK: - Lifecycle methods
+
+extension BaseViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.barStyle = .default
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+        UITextField.appearance().tintColor = UIColor.systemTeal
+    }
+}
+
+// MARK: - Functions
+
+extension BaseViewController {
+    func setupView(_ view: UIView) {
+        self.view.backgroundColor = .red
+        self.view.addSubview(view)
+        //        view.fillSuperviewSafeAreaLayoutGuide()
+        hideKeyboardWhenTappedAround()
+    }
+}
+
+// MARK: - Keyboard handling
+
+extension BaseViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
