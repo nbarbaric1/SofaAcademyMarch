9b567b04fdb1c415dd99c80d1ad797a7198f7d04
Nikola <barbarinikola3@gmail.com>
2022-04-20 23:24:26 +0200

    calling get followers on viewdidload

--- iOS app/SofaAcademyMarch/UIFlow/FollowersScreen/FollowersVC.swift
+++ iOS app/SofaAcademyMarch/UIFlow/FollowersScreen/FollowersVC.swift
@@ -10,6 +10,7 @@ import UIKit
 class FollowersVC: BaseViewController {
     private let followersView = FollowersView()
     var username: String!
+    var followers: [Follower] = []
 }
 
 // MARK: - Lifecycle methods
@@ -26,6 +27,7 @@ private extension FollowersVC {
     func configureVC() {
         setupView(followersView)
         setupBindings()
+        getFollowers()
     }
     
     func setupBindings() {
@@ -36,5 +38,23 @@ private extension FollowersVC {
         let userDetailsVC = UserDetailsVC()
         userDetailsVC.title = "User Details"
         navigationController?.present(userDetailsVC, animated: true)
+    }
+}
+
+private extension FollowersVC{
+    func getFollowers() {
+        NetworkManger.shared.getFollowers(for: username).sink { completion in
+            switch completion {
+                
+            case .finished:
+                break
+            case .failure(_):
+                PopUpManager.shared.presentAlert(title: "Try again", message: "Please check username and try again.", orientation: .horizontal, closures: [("Ok", {})])
+            }
+        } receiveValue: { [weak self] followers in
+            guard let self = self else { return }
+            self.followers = followers
+            print("Followers recieved: ", followers)
+        }.store(in: &subscriptions)
     }
 }
